23c1d2f2dcbb0c79cc08c32ee7d2ab249be53a92
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-01 14:15:17 -0300

    Atualizações

--- interactions.js
+++ interactions.js
@@ -1,5 +1,5 @@
 function initScrollSuave() {
-    const links = document.querySelectorAll('.menuJs a');
+    const links = document.querySelectorAll('a');
 
     function scrollParaSection(event) {
         event.preventDefault();
