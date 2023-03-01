5fb7a2acbd95d7d9ceb53ddd23319265098741ae
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-01 18:03:01 -0300

    Update interactions.js

--- interactions.js
+++ interactions.js
@@ -1,17 +1,15 @@
 function initScrollSuave() {
-    const links = document.querySelectorAll('.menuJs a');
-
+    const links = document.querySelectorAll('.menuJs a'); // Seleciona todos os links do menu de navegação.
     function scrollParaSection(event) {
-        event.preventDefault();
-        const href = event.target.getAttribute('href');
-        const section = document.querySelector(href);
-        
-        section.scrollIntoView({
+        event.preventDefault(); //Desliga o comportamento padrão dos links, ou seja, a navegação interna.
+        const href = event.target.getAttribute('href'); // Pegando o href dos links, ids ligados as sections
+        const section = document.querySelector(href); // Recebendo o href dos links
+        section.scrollIntoView({ // Método para fazer o scroll suave na página, indo diretamente para a section passada no href.
             behavior: "smooth",
         });
     }
 
-    links.forEach((link) => {
+    links.forEach((link) => { // Cria a função com evento de click
         link.addEventListener('click', scrollParaSection);
     });
 }
@@ -19,16 +17,16 @@ initScrollSuave();
 
 function initContentShow() {
     function animationScroll(){
-        const sections = document.querySelectorAll('.flex_conteiner')
-        sections.forEach((section) => {
-            const topSection = section.getBoundingClientRect().top - 300;
-            if(topSection <= 0){
+        const sections = document.querySelectorAll('.flex_conteiner') // Seleciona todas as sections da página.
+        sections.forEach((section) => { // Método forEach
+            const topSection = section.getBoundingClientRect().top - 300; // Pega o topo da section selecionada com -300 para aparecer antes.
+            if(topSection <= 0){ // Quando essa altura for <= a 0, adiciona a classe para aparecer o conteúdo da section.(animado com CSS)
                 section.classList.add('animationSection')
             }
         })
     }
     
-    window.addEventListener('scroll', animationScroll)
+    window.addEventListener('scroll', animationScroll) // Função com evento de scroll em toda a página.
 }
 initContentShow();
 
